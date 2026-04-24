75f7f348e57ba1f5237ffd309bf1042757c1f4ae
hrvthlevi <horvathleventeroland@gmail.com>
2026-04-24 11:22:47 +0200

    Program osztaly kesz, teszt futtatas -> kesz

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,10 +1,18 @@
 
 package main;
 
+import Modell.Asztal;
+import Modell.Sor;
+
 public class Program {
 
     public static void main(String[] args) {
-      
+        Asztal asztal = new Asztal();
+        asztal.sorHozzaad(new Sor());
+        asztal.sorHozzaad(new Sor(5.2, 500, false));
+        
+        System.out.println(asztal.toString());
+        System.out.println(asztal.getAsztalRajz());
     }
     
 }
